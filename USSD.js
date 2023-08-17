4d5d44ab200be671af772d7bd515fb7c0c9569ab
Muhammad Najeeb <yusnaj21@gmail.com>
2023-08-17 18:06:21 +0100

    Fixed transaction channels

--- USSD.js
+++ USSD.js
@@ -1,6 +1,7 @@
 const pinCode = "*901#";
 let accountBalance = 5000;
 const userPin = 1111;
+let account, amount, confirmation, bankName;
 alert("Dial *901# to perform a USSD transaction");
 let accessCode = prompt("Enter USSD Code here");
 if (accessCode === pinCode) {
@@ -12,28 +13,34 @@ if (accessCode === pinCode) {
     5.Deactivate Account`);
 
   if (transaction === 1) {
-    let affirm = confirm(`₦6.98 network charge will apply for this transaction?
-        Would you like to proceed?`);
+    let affirm = confirm(`
+      ₦6.98 network charge will apply for this transaction.
+      Would you like to proceed?`);
     if (affirm === true) {
-      alert(`Your account balance is ₦${accountBalance}.
-            Thank you!`);
+      alert(`
+        Your account balance is ₦${accountBalance}.
+        Thank you!`);
     } else {
-      alert("Thank you for choosing IDAN Bank");
+      alert("Thank you for banking with us");
     }
   } else if (transaction === 2) {
     let bankType = +prompt(`Choose bank type
     1. Access Transfer
     2. Transfer to other banks`);
     if (bankType === 1) {
-      +prompt("Enter amount");
-      let confirmation = +prompt("Enter your Secret pin");
-      if (confirmation === userPin) {
-        alert(`Transfer Complete`);
+      account = prompt("Enter account number");
+      amount = +prompt("Enter amount");
+      confirmation = +prompt("Enter your Secret pin");
+      if (confirmation === userPin && amount <= accountBalance) {
+        alert(`
+        You have transferred ₦${amount} to ${account}
+        Your account balance is: ₦${accountBalance - amount}
+        Thank you.`);
       } else {
         alert("Transaction failed");
       }
     } else {
-      let bankName = +prompt(`Choose bank name
+      bankName = +prompt(`Choose bank name
         1. GT Bank
         2. Zenith Bank
         3. UBA Bank
@@ -41,28 +48,48 @@ if (accessCode === pinCode) {
         5. First Bank
         99. Others`);
 
+      if (bankName === 1) {
+        bankName = "GT Bank";
+      } else if (bankName === 2) {
+        bankName = "Zenith Bank";
+      } else if (bankName === 3) {
+        bankName = "UBA Bank";
+      } else if (bankName === 4) {
+        bankName = "Union Bank";
+      } else if (bankName === 5) {
+        bankName = "First Bank";
+      } else if (bankName === 99) {
+        bankName = "";
+      }
       switch (bankName) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-          let accountNum = +prompt("Enter account number");
+        case "GT Bank":
+        case "Zenith Bank":
+        case "UBA Bank":
+        case "Union Bank":
+        case "First Bank":
+          account = +prompt("Enter account number");
           let amount = +prompt("Enter amount");
           let confirmation = +prompt("Enter your Secret pin");
           if (confirmation === userPin) {
-            alert(`Transfer Complete
-            You have transefered ₦${amount} to ${accountNum}`);
+            alert(`
+            Transfer Complete
+            You have transefered ₦${amount} to ${bankName}
+            with account number ${account}
+            Thank you.`);
           } else {
             alert("Transaction failed");
           }
           break;
         case 99:
-          let selectBank = prompt("Enter bank name");
+          bankName = prompt("Enter bank name");
+          account = +prompt("Enter account number");
           confirmation = +prompt("Enter your Secret pin");
           if (confirmation === userPin) {
-            alert(`Transfer Complete
-            You have transefered ₦${amount} to ${selectBank}`);
+            alert(`
+            Transfer Complete
+            You have transefered ₦${amount} to ${bankName}
+            with ${account}
+            Thank you.`);
           } else {
             alert("Transaction failed");
           }
@@ -73,10 +100,14 @@ if (accessCode === pinCode) {
     1. Self
     2. Others`);
     if (self === 1) {
-      +prompt(`Enter amount`);
+      amount = +prompt(`Enter amount`);
       let confirmation = +prompt("Enter your Secret pin");
       if (confirmation === userPin) {
-        alert(`Transfer Complete`);
+        alert(`
+        Transfer Complete
+        You recharged your line with ₦${amount} airtime
+        Your account balance is: ₦${accountBalance - amount}
+        Thank you.`);
       } else {
         alert("Transaction failed");
       }
@@ -90,7 +121,7 @@ if (accessCode === pinCode) {
       const rechargeAmount = +prompt("Enter recharge amount");
       let confirmation = +prompt("Enter your Secret pin");
       if (confirmation === userPin) {
-        alert(`${userNum} has been recharged ${rechargeAmount}
+        alert(`${userNum} has been recharged ₦${rechargeAmount}
         Thank you.`);
       } else {
         alert("Transaction failed");
@@ -105,10 +136,12 @@ if (accessCode === pinCode) {
   } else {
     alert("Invalid Transaction");
   }
+} else if (accessCode !== pinCode) {
+  confirm(`
+  Welcome to Access Bank USSD Banking.
+  ₦6.98 network change will apply to your 
+  account for services on this channel.
+  Press 901 to accept or 2 to reject`);
 } else {
-  confirm(
-    `Welcome to Access Bank USSD Banking.
-     ₦6.98 network change will apply to your account for banking services on this channel.
-     Press 901 to accept or 2 to reject`
-  );
+  alert("Thanking you for choosing Access bank");
 }
